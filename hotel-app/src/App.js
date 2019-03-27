d93918d45920ff23ecc26fc3c1fac7e75839b9b8
ramy.med7t@gmail.com <ramy.med7t@gmail.com>
2019-03-27 22:39:45 +0200

    start using scss

--- hotel-app/src/App.js
+++ hotel-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import Hotels from './components/Hotels'
 class App extends Component {
   render() {
--- hotel-app/src/components/Hotels.js
+++ hotel-app/src/components/Hotels.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import StarRatingComponent from 'react-star-rating-component';
-import './hotels.css'
+import './hotels.scss'
 class Hotels extends Component {
     state = {
         hotels: []
@@ -16,16 +16,26 @@ class Hotels extends Component {
   render() {
     const hotelItems = this.state.hotels.map( hotel =>  (
         <div className="hotel-card" key={hotel.id}>
-        <img src={require(`../utilities/${hotel.images[0]}.jpg`)} alt="{hotel.name}"/>
-            <h2>{hotel.name}</h2>
-            <StarRatingComponent 
-                name="rate1" 
-                starCount={5}
-                value={hotel.rating}
-            />
-            <p>{hotel.price_category}</p>
-            <p>{hotel.distance_to_venue}</p>
-            <p>{hotel.amenities.join(' ')}</p>
+            <div className='image-container'>
+                <img 
+                    src={require(`../utilities/${hotel.images[0]}.jpg`)} 
+                    alt="{hotel.name}"
+                />
+            </div>
+            <div className='main-information'>
+                    <h3 className='hotel-name'>{hotel.name}</h3>
+                    <StarRatingComponent 
+                        name="rate1" 
+                        starCount={5}
+                        value={hotel.rating}
+                    />
+                    <p className="hotel-amenities"><span>amenities:</span>{hotel.amenities.join(' ')}</p>
+                </div>
+                <div className='secondly-information'>
+                    <p>{hotel.distance_to_venue}</p>
+                    <p>{hotel.price_category}</p>
+                    <button>view hotel</button>
+                </div>    
         </div>
     ))
     return (
